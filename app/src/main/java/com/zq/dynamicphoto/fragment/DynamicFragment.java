91f21c0709feff454b42aeb19ca712977c7689b9
Chendida <495518592@qq.com>
2018-06-08 15:35:39 +0800

    添加首页4个碎片，我的界面的布局

---
+++ app/src/main/java/com/zq/dynamicphoto/fragment/DynamicFragment.java
@@ -0,0 +1,31 @@
+package com.zq.dynamicphoto.fragment;
+
+import android.view.View;
+import com.zq.dynamicphoto.R;
+import com.zq.dynamicphoto.base.BaseFragment;
+import com.zq.dynamicphoto.base.BasePresenter;
+
+/**
+ * 首页
+ */
+public class DynamicFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_dynamic;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/zq/dynamicphoto/fragment/LiveFragment.java
@@ -0,0 +1,32 @@
+package com.zq.dynamicphoto.fragment;
+
+import android.view.View;
+import com.zq.dynamicphoto.R;
+import com.zq.dynamicphoto.base.BaseFragment;
+import com.zq.dynamicphoto.base.BasePresenter;
+/**
+ * 直播
+ */
+public class LiveFragment extends BaseFragment {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_live;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected BasePresenter createPresenter() {
+        return null;
+    }
+
+}
